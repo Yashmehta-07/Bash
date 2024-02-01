fba1c9c78b0dd77bfc58a2307e81b7d68a7397ff
Yashmehta-07 <yash.mehta@remotestate.com>
2024-02-01 13:28:42 +0530

    switch case ques

---
+++ swich case1.sh
@@ -0,0 +1,28 @@
+case $choice in
+    1)
+        read -p "Enter Number1: " number1
+        read -p "Enter Number2: " number2
+        echo Answer=$(( $number1 + $number2 ))
+        ;;
+    2)
+        read -p "Enter Number1: " number1
+        read -p "Enter Number2: " number2
+        echo Answer=$(( $number1 - $number2 ))
+        ;;
+
+    3)
+        read -p "Enter Number1: " number1
+        read -p "Enter Number2: " number2
+        echo Answer=$(( $number1 * $number2 ))
+        ;;
+    4)
+        read -p "Enter Number1: " number1
+        read -p "Enter Number2: " number2
+        echo Answer=$(( $number1 / $number2 ))
+        ;;
+    5)
+        break
+        ;;
+  esac
+
+done
